9aaa5352432ea5d43731a2cbf3b59d2e48e304fe
Bereket Tsegay <eketei23@yahoo.com>
2025-09-06 08:14:31 +0400

    adding the menuitem

---
+++ resources/js/pages/partials/menuItems.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <ul class="space-y-2 font-medium">
+            <li>
+               <router-link :to="href" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <i :class='fa fa-{icon}'></i>
+                  <span class="ms-3">{{label}}</span>
+               </router-link>
+            </li>
+        </ui>
+</template>
+<script>
+export default {
+
+    props:[
+        
+    ]
+    
+}
+</script>
+<style lang="">
+    
+</style>
