a5346d9db1c7b9cddcb2d8f753876a759aecbd59
Bereket diverge <bereket@diverge.world>
2025-09-29 23:01:49 +0400

    mekina

--- resources/js/components/userRoleUpdate.vue
+++ resources/js/components/userRoleUpdate.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>
 
-    </div>
+        <div class="grid gap-2">
+            <SelectField
+             :errors="errors.name"
+             label="Roles"
+             :options="rols"
+             optionLabel="name"
+             optionValue="id"
+             placeholder="Role"
+             isMultiple="true"
+             :modelValue="user.role_id??''"
+             icon="briefcase"
+             @input="($event)=> user.role_id = $event.target.value">
+            </SelectField>
+        </div>
 </template>
 <script>
+import SelectField from './selectField.vue';
 export default {
-
+    components:{SelectField},
+    name:'userRoleUpdate',
+    props:['user'],
+    data() {
+        return {
+            errors:[],
+            rols:[],
+        }
+    },
+    mounted() {
+        this.getRols();
+    },
+    methods:{
+        getRols(){
+            //fetch roles from api and assign to rols
+            axios.get('/api/admin/roles/roles')
+            .then(res => this.rols = res.data)
+            .catch(err => this.message = err.response.message);
+        }
+    },
 }
 </script>
 <style>
